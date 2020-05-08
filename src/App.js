4277ee0ea7f845922a8c3557280c571b6603442a
Timothy Oliver <oliverwakogi@live.com>
2020-05-08 12:55:38 +0300

    Complete UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx'
+import PricingPage from './components/pricing-page/PricingPage'
+
+const useStyles = makeStyles({
+  app: {
+    height: '100%'
+  }
+})
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={clsx('App', classes.app)}>
+      <PricingPage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
